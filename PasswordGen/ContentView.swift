f8b1ff664412c5c0013db973a0e776e644ab0ffa
Tim Randall <trtimrandall@gmail.com>
2021-05-30 20:40:30 +1000

    Initial Commit

---
+++ PasswordGen/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PasswordGen
+//
+//  Created by Tim Randall on 30/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PasswordGen/PasswordGenApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordGenApp.swift
+//  PasswordGen
+//
+//  Created by Tim Randall on 30/5/21.
+//
+
+import SwiftUI
+
+@main
+struct PasswordGenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
